ed9886d6f6584b0cba4882fa504648652b377afa
roieLandsman <82931855+roieLandsman@users.noreply.github.com>
2022-01-03 16:43:08 +0200

    Update build.gradle.kts

--- kotlin-example/lib/build.gradle.kts
+++ kotlin-example/lib/build.gradle.kts
@@ -2,13 +2,14 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.0"
     `java-library`
-    `maven-publish`
+    `maven-publish` // publishing plugin a must
 }
 
 group = "experiment.ssi.kotlin_test" // please do not change we don't want to mess the repository
 version = "1.0-SNAPSHOT" // change to see that you actually publish something new
 
-val artifactory_url: String? by properties
+// get properties from gradle/properties. names must match to those in the file.
+val artifactory_url: String? by properties 
 val artifactory_publish_url: String? by properties
 val artifactory_username: String? by properties
 val artifactory_password: String? by properties
@@ -28,6 +29,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
+// copy block as it is. notice the parameters names, must match the definition above. 
 publishing {
     publications {
         create<MavenPublication>("maven") {
